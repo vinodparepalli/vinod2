d458b23590c47d2332beb65f6ab0d33b5a4f5983
vinodparepalli <113435438+vinodparepalli@users.noreply.github.com>
2022-09-14 14:56:04 +0530

    Update HELLO.js

--- HELLO.js
+++ HELLO.js
@@ -1,7 +1,7 @@
 const app=require("express")();
 const PORT=process.env.PORT || 3000;
 app.get("",(req,res)=>{
-    res.send("hello world");
+    res.send("vinod parepalli");
 });
 app.listen(PORT, ()=>{
     console.log('app up at port ${PORT}');
